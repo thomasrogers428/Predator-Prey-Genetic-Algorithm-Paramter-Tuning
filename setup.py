777e2f5cd24af1c5d066941544a9823946da23e9
Tommy Rogers <tomrog428@yahoo.com>
2022-11-03 14:47:52 -0400

    Initial Commit

--- setup.py
+++ setup.py
@@ -6,9 +6,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['simple_motion'],
+    packages=['ppgapt'],
     package_dir={'': 'src'}
 )
 
 setup(**setup_args)
-
